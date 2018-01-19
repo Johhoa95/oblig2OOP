9961aa218a5b03eea178b9046def99bb4e8d9f9b
John Magnus Hoaas <Hoaas95@hotmail.com>
2018-01-19 15:24:07 +0100

    Kjørte koden for å se om den fungerte

--- src/no/hiof/johnmh/oblig2/Oppgave2.java
+++ src/no/hiof/johnmh/oblig2/Oppgave2.java
@@ -3,6 +3,16 @@ package no.hiof.johnmh.oblig2;
 public class Oppgave2 {
     public static void main(String[] args) {
         TVSerie show1 = new TVSerie("csi", "1995");
+
+        Episode killing = new Episode(1, 1, "Killing", 90);
+
+        Episode detectiv = new Episode(2, 1, "Detectiv", 90);
+
+        show1.leggTilEpisoder(killing);
+        show1.leggTilEpisoder(detectiv);
+
+        System.out.println(show1.getTittel() + " " + show1.getUtgivelsesDato());
+
         
 
     }
--- src/no/hiof/johnmh/oblig2/episode.java
+++ src/no/hiof/johnmh/oblig2/episode.java
@@ -1,18 +1,18 @@
 package no.hiof.johnmh.oblig2;
 
 public class Episode {
-    private String episodeNummer;
-    private String sesong;
+    private int episodeNummer;
+    private int sesong;
     private String tittel;
     private int spilleTid;
 
-    public Episode(String episodeNummer, String sesong, String tittel) {
+    public Episode(int episodeNummer, int sesong, String tittel) {
         this.episodeNummer = episodeNummer;
         this.sesong = sesong;
         this.tittel = tittel;
     }
 
-    public Episode(String episodeNummer, String sesong, String tittel, int spilleTid) {
+    public Episode(int episodeNummer, int sesong, String tittel, int spilleTid) {
         this.episodeNummer = episodeNummer;
         this.sesong = sesong;
         this.tittel = tittel;
