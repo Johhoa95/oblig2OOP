846e10fe07fd5cdb96b667da9c9f67d6ffa85aa1
John Magnus Hoaas <Hoaas95@hotmail.com>
2018-01-19 14:17:19 +0100

    La til variabler i klassen

--- src/no/hiof/johnmh/oblig2/TVSerie.java
+++ src/no/hiof/johnmh/oblig2/TVSerie.java
@@ -1,5 +1,13 @@
 package no.hiof.johnmh.oblig2;
 
+import java.time.LocalDate
+
 public class TVSerie {
-    private String 
+    private String tittel;
+    private String beskrivelse;
+    private int dag;
+    private int maaned;
+    private int aar;
+
+    
 }
