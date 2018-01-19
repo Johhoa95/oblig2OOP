91c5fb96f2ec933b3a2cd551c4b35e863bac9e6f
John Magnus Hoaas <Hoaas95@hotmail.com>
2018-01-19 14:58:19 +0100

    Skiftet til String, la til get/set/konstruktør og opprettet en testfil

---
+++ src/no/hiof/johnmh/oblig2/Oppgave2.java
@@ -0,0 +1,11 @@
+package no.hiof.johnmh.oblig2;
+
+public class Oppgave2 {
+    public static void main(String[] args) {
+        TVSerie show1 = new TVSerie("csi", "1995");
+        
+
+    }
+
+
+}
--- src/no/hiof/johnmh/oblig2/TVSerie.java
+++ src/no/hiof/johnmh/oblig2/TVSerie.java
@@ -1,13 +1,35 @@
 package no.hiof.johnmh.oblig2;
 
-import java.time.LocalDate
+import java.util.ArrayList;
 
 public class TVSerie {
     private String tittel;
     private String beskrivelse;
-    private int dag;
-    private int maaned;
-    private int aar;
+    private String utgivelsesDato;
+    private ArrayList<Episode> episoder = new ArrayList<>();
 
-    
+    public void leggTilEpisoder(Episode episode){
+        this.episoder.add(episode);
+    }
+
+    public TVSerie(String tittel, String utgivelsesDato) {
+        this.tittel = tittel;
+        this.utgivelsesDato = utgivelsesDato;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public void setTittel(String tittel) {
+        this.tittel = tittel;
+    }
+
+    public String getUtgivelsesDato() {
+        return utgivelsesDato;
+    }
+
+    public void setUtgivelsesDato(String utgivelsesDato) {
+        this.utgivelsesDato = utgivelsesDato;
+    }
 }
